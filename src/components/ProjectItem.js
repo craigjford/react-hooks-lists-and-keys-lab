69798570dd0b539c443f0467ab3d99ab7f665a7e
Craig Ford <craig_ford@icloud.com>
2022-04-06 13:10:23 -0400

    Complete

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,20 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  console.log('in ProjectItem ', {name})
+  console.log('in ProjectItem ', {about})
+  console.log('in ProjectItem ', {technologies})
+
+  const techList = technologies.map(tech => 
+      <span>{tech}</span>
+  );
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
         {/* render a <span> for each technology in the technologies array */}
+        {techList}
       </div>
     </div>
   );
